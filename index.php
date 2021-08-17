8632d5d97ae818ff17336f90a14acd7151b14b62
UchihaLucasVieira <lucastotti2008@hotmail.com>
2021-08-17 20:48:14 -0300

    Adicionei mais informações do curso

--- index.php
+++ index.php
@@ -3,5 +3,8 @@
 echo "Ola Mundo!";
 echo "Modificação Simples de PHP7!";
 echo "Curso da udemy!";
+echo "Fazendo Alteração Do PC";
+echo "Para o Servidor Remoto";
+echo "Arquivos Alterados com sucesso";
 
 ?>
